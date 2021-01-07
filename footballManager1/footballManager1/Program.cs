fecff1f9683df9a3c9c4c4111852f8e321ee902d
alexandar004 <73309925+alexandar004@users.noreply.github.com>
2021-01-07 22:21:30 +0200

    initial cimmit

---
+++ footballManager1/footballManager1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace footballManager1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var team1 = new Team();
+            team1.Name = "Chelsea";
+            team1.Strenght = 7;
+            team1.Manager = new Manager()
+            {
+                Name = "Gosho",
+                YearExpririence = 10
+            };
+            team1.HomeStadium = "Stanford";
+
+
+            var team2 = new Team();
+            Console.WriteLine("Enter the name of first team");
+            team2.Name = Console.ReadLine();
+            Console.WriteLine("Enter the strength of the first team");
+            team2.Strenght = int.Parse(Console.ReadLine());
+            team2.Manager = new Manager()
+            {
+                Name = "Gosho",
+                YearExpririence = 10
+            };
+            team2.HomeStadium = "Stanford";
+
+
+            //var manager = new Manager() { Name = "Pesho", Loses = 3, Wins = 34, YearExpririence = 7 };
+            //var team2 = new Team("Manchester", 8, "Anfield", manager);
+
+            PlayGame(team1, team2, "Stanford");
+        }
+
+    static void PlayGame(Team firstTeam, Team secondTeam, string stadium)
+    {
+            double firstTeamScore = 0;
+            double secondTeamScore = 0;
+
+            if (firstTeam.HomeStadium == stadium)
+            {
+                firstTeamScore++;
+            }
+            else if( secondTeam.HomeStadium == stadium)
+            {
+                secondTeamScore++;
+            }
+
+            firstTeamScore += firstTeam.Strenght;
+            secondTeamScore += secondTeam.Strenght;
+
+            // Add Manager experience
+            firstTeamScore += firstTeam.Manager.YearExpririence * 0.2;
+            secondTeamScore += secondTeam.Manager.YearExpririence * 0.2;
+
+            // Add random number 
+            var random = new Random();
+            firstTeamScore += random.Next(1, 10) * 0.3;
+            secondTeamScore += random.Next(1, 10) * 0.3;
+
+
+            if (firstTeamScore > secondTeamScore)
+            {
+                Console.WriteLine($"The winner is {firstTeam.Name} ");
+            }
+            else if(firstTeamScore < secondTeamScore)
+            {
+                Console.WriteLine($"The winner is {secondTeam.Name} ");
+            }
+            else
+            {
+                Console.WriteLine($"The result are equal");
+            } 
+        }
+}
+}
---
+++ footballManager1/footballManager1/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace footballManager1
+{
+    class Team
+    {
+        public Team()
+        {
+
+        }
+
+        public Team(string name, int strenght, string stadium, Manager manager) : this()
+        {
+            this.Name = name;
+            this.Strenght = strenght;
+            this.HomeStadium = stadium;
+            this.Manager = manager;
+        }
+
+        public string Name { get; set; }
+        public int Strenght { get; set; }
+        public string HomeStadium { get; set; }
+        public Manager Manager { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
