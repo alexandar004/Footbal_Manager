104db9c4b26d62df9a926f053587a573cbc25f47
alexandar004 <73309925+alexandar004@users.noreply.github.com>
2021-06-09 12:25:30 +0300

    Add files via upload

--- footballManager1/Program.cs
+++ footballManager1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace footballManager1
 {
@@ -8,34 +10,58 @@ namespace footballManager1
         {
             var teams = new Team[4];
 
-            teams[0] = new Team() { Name = "Chelsea", Strenght = 5 , HomeStadium="Stanford", Manager = new Manager() 
+            teams[0] = new Team()
             {
-                Name= "Frank Lampard",
-                Wins= 67,
-                YearExpririence = 5,
-                Loses = 21,
-            }};
-            teams[1] = new Team() { Name = "FC Bayern Munchen", Strenght = 6, HomeStadium = "Alianz Arena", Manager = new Manager()
+                Name = "Chelsea",
+                Strenght = 1,
+                HomeStadium = "Stanford Bridge",
+                Manager = new Manager()
+                {
+                    Name = "Frank Lampard",
+                    Wins = 67,
+                    YearExpririence = 5,
+                    Loses = 21,
+                }
+            };
+            teams[1] = new Team()
             {
-                Name = "Hans-Dieter Flick",
-                Wins = 56,
-                YearExpririence = 6,
-                Loses = 24,
-            }};
-            teams[2] = new Team() { Name = "Real Madrid", Strenght = 4, HomeStadium = "Santiago", Manager = new Manager()
-            { 
-                Name = "Zinedine Zidane",
-                Wins = 70,
-                YearExpririence = 4,
-                Loses = 21,
-            } };
-            teams[3] = new Team() { Name = "PSG", Strenght = 7, HomeStadium = "Parc des Princes", Manager = new Manager()
-            { 
-                Name = "Mauricio Pochettino",
-                Wins = 76,
-                YearExpririence = 5,
-                Loses = 17,
-            } };
+                Name = "FC Bayern Munchen",
+                Strenght = 2,
+                HomeStadium = "Alianz Arena",
+                Manager = new Manager()
+                {
+                    Name = "Hans-Dieter Flick",
+                    Wins = 56,
+                    YearExpririence = 1,
+                    Loses = 24,
+                }
+            };
+            teams[2] = new Team()
+            {
+                Name = "Real Madrid",
+                Strenght = 2,
+                HomeStadium = "Santiago",
+                Manager = new Manager()
+                {
+                    Name = "Zinedine Zidane",
+                    Wins = 70,
+                    YearExpririence = 4,
+                    Loses = 21,
+                }
+            };
+            teams[3] = new Team()
+            {
+                Name = "PSG",
+                Strenght = 2,
+                HomeStadium = "Parc des Princes",
+                Manager = new Manager()
+                {
+                    Name = "Mauricio Pochettino",
+                    Wins = 76,
+                    YearExpririence = 5,
+                    Loses = 17,
+                }
+            };
 
 
             Console.WriteLine("Choose a team: ");
@@ -44,10 +70,10 @@ namespace footballManager1
                 Console.WriteLine($"Select {i + 1} for {teams[i].Name} ");
             }
 
-            var selectedTeamIndex = int.Parse(Console.ReadLine()) -1;
+            var selectedTeamIndex = int.Parse(Console.ReadLine()) - 1;
 
             Console.WriteLine("Choose a oponent team: ");
-            
+
             for (int i = 0; i < teams.Length; i++)
             {
                 if (i != selectedTeamIndex)
@@ -71,7 +97,7 @@ namespace footballManager1
 
         }
         static void PlayGame(Team firstTeam, Team secondTeam, string stadium)
-    {
+        {
             double firstTeamScore = 0;
             double secondTeamScore = 0;
 
@@ -80,7 +106,7 @@ namespace footballManager1
             {
                 firstTeamScore++;
             }
-            else if( secondTeam.HomeStadium == stadium)
+            else if (secondTeam.HomeStadium == stadium)
             {
                 secondTeamScore++;
             }
@@ -94,15 +120,15 @@ namespace footballManager1
 
             // Add random number 
             var random = new Random();
-            firstTeamScore += random.Next(1, 10) * 0.3;
-            secondTeamScore += random.Next(1, 10) * 0.3;
+            firstTeamScore += random.Next(1, 5) * 0.2;
+            secondTeamScore += random.Next(1, 5) * 0.2;
 
 
             if (firstTeamScore > secondTeamScore)
             {
                 Console.WriteLine($"The winner is {firstTeam.Name} ");
             }
-            else if(firstTeamScore < secondTeamScore)
+            else if (firstTeamScore < secondTeamScore)
             {
                 Console.WriteLine($"The winner is {secondTeam.Name} ");
             }
@@ -112,6 +138,31 @@ namespace footballManager1
             }
             Console.WriteLine($"The score is {Math.Round(firstTeamScore)} : {Math.Round(secondTeamScore)}.");
 
+            List<Player> players = new List<Player>
+            {
+                new Player() { Name = "Cristiano Ronaldo", Speed = 7, GoalsCount = 45, numberOfPlayer = 1 },
+                new Player() { Name = "Lionel Messi", Speed = 7, GoalsCount = 34, numberOfPlayer = 2 },
+                new Player() { Name = "Valeri Bozhinov", Speed = 4, GoalsCount = 12, numberOfPlayer = 3 },
+                new Player() { Name = "Dimitar Berbatov", Speed = 5, GoalsCount = 15, numberOfPlayer = 4 },
+                new Player() { Name = "Hristo Stoichkov", Speed = 5, GoalsCount = 50, numberOfPlayer = 5 }
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("Goals:");
+
+            double resultScore = firstTeamScore + secondTeamScore;
+            int randomGoals = random.Next(1, 4);
+
+            for (int i = 0; i <  resultScore; i++)
+            {
+                int min = random.Next(90);
+
+                int randomNumber2 = random.Next(1, 5);
+
+                var randomPlayer = players.FirstOrDefault(p => p.numberOfPlayer == randomNumber2);
+
+                Console.WriteLine($"min - {min} notes {randomPlayer.Name}");
+            }
         }
     }
 }
